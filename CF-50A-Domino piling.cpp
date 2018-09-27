eb00027937dc4704989bbfe70003676e6e83cf31
Ujjval Patel <30762887+Ujjval-Patel@users.noreply.github.com>
2018-09-27 11:20:58 +0530

    Create CF-50A-Domino piling.cpp

---
+++ CF-50A-Domino piling.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <queue>
+
+using namespace std;
+#define ll long long
+
+
+int main() 
+{
+    ll n,m;
+    cin>>n>>m;
+    cout<<(n*m)/2<<endl;
+    return 0;
+}
